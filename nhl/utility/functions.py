f145d2b62fdc7a19d06093233bd89f799d2594bb
max tixador <maxtixador@gmail.com>
2023-12-29 00:26:14 -0500

    updating

--- nhl/utility/functions.py
+++ nhl/utility/functions.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 from .decorators import *
 from datetime import datetime 
-
+from bs4 import BeautifulSoup
 
 # @timer
 def fetch_teams_json(date = "now"):
@@ -31,7 +31,7 @@ def fetch_teams_json(date = "now"):
     response.raise_for_status()
     return response.json()
 
-@timer
+# @timer
 def fetch_play_by_play_json(game_id: int):
     """
     Connects to the NHL API to get the data for a given game.
@@ -252,6 +252,7 @@ def get_standings(date = "now"):
                             
     return df
 
+@timer
 def get_pbp(game_id: int):
     """
     Gets the play-by-play for a given game.
@@ -300,6 +301,30 @@ def get_pbp(game_id: int):
     # Add home/away indicator
     df.loc[df['eventTeam'].notnull(),'is_home'] = (df['eventTeam'] == game_dict.get('homeTeam', {}).get('abbrev', "")).astype(int)
 
+    df['event_player1_Id'] = df[['details.winningPlayerId', 'details.hittingPlayerId', 'details.shootingPlayerId', 'details.scoringPlayerId', 'details.committedByPlayerId', 'details.playerId']].bfill(axis=1).iloc[:, 0]
+
+    df['event_player2_Id'] = df[['details.losingPlayerId',  'details.blockingPlayerId','details.hitteePlayerId', 'details.assist1PlayerId', 'details.drawnByPlayerId']].bfill(axis=1).iloc[:, 0]
+
+    df['event_player3_Id'] = df['details.assist2PlayerId']
+
+    df = df.drop(columns=['details.winningPlayerId', 'details.losingPlayerId', 'details.hittingPlayerId',
+                            'details.shootingPlayerId', 'details.scoringPlayerId', 'details.committedByPlayerId',
+                            'details.blockingPlayerId','details.hitteePlayerId', 'details.assist1PlayerId',
+                            'details.drawnByPlayerId', 'details.assist2PlayerId',
+                            'details.eventOwnerTeamId', 'details.scoringPlayerTotal', 'details.assist1PlayerTotal',
+                            'details.assist2PlayerTotal', 'situationCode','eventId', 'details.playerId'])
+
+    df = df.sort_values(by=['elapsedTime'], ascending=True).reset_index(drop=True)
+
+    df = df.rename(columns={'periodDescriptor.number': 'period'})
+
+
+    # Remove 'details.' and 'periodDescriptor.' prefixes from column names
+    df.columns = df.columns.str.replace('details.', '').str.replace('periodDescriptor.', '')
+
+    # Now, the column names will be updated with the prefixes removed
+
+
     # #Fix score when goal
     # df.loc[(df['event'] == 'goal') & (df['is_home'] == 1), 'details.homeScore'] = df['details.homeScore'] + 1
     # df.loc[(df['event'] == 'goal') & (df['is_home'] == 0), 'details.awayScore'] = df['details.awayScore'] + 1
@@ -307,4 +332,133 @@ def get_pbp(game_id: int):
 
 
     return df
+
+# @timer
+def fetch_html_shifts(game_id=2023020069, season=None, pbp_json=None):
+    ''' 
+    Fetches shifts data from the NHL API and returns a DataFrame with the data.
+    ----
+    :param game_id: The game ID of the game to fetch shifts for.
+    :param season: The season of the game. If not provided, it will be fetched from the API.
+    :param pbp_json: The play-by-play JSON for the game. If not provided, it will be fetched from the API.
+    :return: A DataFrame containing the shifts data for the game.
+    '''
+
+    # pbp_json = fetch_play_by_play_json(game_id) if pbp_json is None else pbp_json
+    # rosters = fetch_game_rosters(game_id)
+
+    season = f"{str(game_id)[:4]}{int(str(game_id)[:4]) + 1}" if season is None else season
+
+
+    ### HOME SHIFTS ###
+    url = SHIFT_REPORT_HOME_ENDPOINT.format(season=season, game_id=str(game_id)[4:])
+    page = (requests.get(url))
+    soup = BeautifulSoup(page.content.decode('ISO-8859-1'), 'html.parser', multi_valued_attributes = None)
+    found = soup.find_all('td', {'class':['playerHeading + border', 'lborder + bborder']})
+    if len(found)==0:
+        raise IndexError('This game has no shift data.')
+    thisteam = soup.find('td', {'align':'center', 'class':'teamHeading + border'}).get_text()
     
+
+    players = dict()
+    for i in range(len(found)):
+        line = found[i].get_text()
+        if ', ' in line:
+            name = line.split(',')
+            number = name[0].split(' ')[0].strip()
+            last_name =  name[0].split(' ')[1].strip()
+            first_name = name[1].strip()
+            full_name = first_name + " " + last_name
+            players[full_name] = dict()
+            players[full_name]['number'] = number
+            players[full_name]['name'] = full_name
+            players[full_name]['shifts'] = []
+        else:
+            players[full_name]['shifts'].extend([line])
+
+    alldf = pd.DataFrame()
+
+    for key in players.keys(): 
+        length = int(len(np.array((players[key]['shifts'])))/5)
+        df = pd.DataFrame(np.array((players[key]['shifts'])).reshape(length, 5)).rename(
+        columns = {0:'shift_number', 1:'period', 2:'shift_start', 3:'shift_end', 4:'duration'})
+        df = df.assign(name = players[key]['name'],
+                      sweaterNumber = int(players[key]['number']),
+                      team = thisteam,
+                      is_home = 1)
+        alldf = pd.concat([alldf, df], ignore_index=True)
+        
+    home_shifts = alldf
+
+    ### AWAY SHIFTS ###
+    url = SHIFT_REPORT_AWAY_ENDPOINT.format(season=season, game_id=str(game_id)[4:])
+    page = (requests.get(url))
+    soup = BeautifulSoup(page.content.decode('ISO-8859-1'), 'html.parser', multi_valued_attributes = None)
+    found = soup.find_all('td', {'class':['playerHeading + border', 'lborder + bborder']})
+    if len(found)==0:
+        raise IndexError('This game has no shift data.')
+    thisteam = soup.find('td', {'align':'center', 'class':'teamHeading + border'}).get_text()
+    
+
+    players = dict()
+    for i in range(len(found)):
+        line = found[i].get_text()
+        if ', ' in line:
+            name = line.split(',')
+            number = name[0].split(' ')[0].strip()
+            last_name =  name[0].split(' ')[1].strip()
+            first_name = name[1].strip()
+            full_name = first_name + " " + last_name
+            players[full_name] = dict()
+            players[full_name]['number'] = number
+            players[full_name]['name'] = full_name
+            players[full_name]['shifts'] = []
+        else:
+            players[full_name]['shifts'].extend([line])
+
+    alldf = pd.DataFrame()
+
+    for key in players.keys(): 
+        length = int(len(np.array((players[key]['shifts'])))/5)
+        df = pd.DataFrame(np.array((players[key]['shifts'])).reshape(length, 5)).rename(
+        columns = {0:'shift_number', 1:'period', 2:'shift_start', 3:'shift_end', 4:'duration'})
+        df = df.assign(name = players[key]['name'],
+                      sweaterNumber = int(players[key]['number']),
+                      team = thisteam,
+                      is_home = 0)
+        alldf = pd.concat([alldf, df], ignore_index=True)
+        
+    away_shifts = alldf
+
+    ### MERGE SHIFTS ###
+    all_shifts = (pd.concat([home_shifts, away_shifts], ignore_index=True)
+                  .drop(columns=['name', 'team']))
+    #               .merge(rosters, how='left', on=['sweaterNumber', 'is_home']))
+
+
+    all_shifts[['startTime', 'startTime_remaning']] = all_shifts['shift_start'].str.split(' / ', expand=True)
+
+    # # Split 'shift_end' column into two columns
+    all_shifts[['endTime', 'endTime_remaning']] = all_shifts['shift_end'].str.split(' / ', expand=True)    
+
+    all_shifts = all_shifts.drop(columns=[ 'startTime_remaning',  'endTime_remaning', 'shift_start', 'shift_end']).replace({'OT':4})
+    
+    str_to_sec = lambda x: int(x.split(':')[0]) * 60 + int(x.split(':')[1])
+
+    all_shifts['period'] = all_shifts['period'].astype(int)
+    all_shifts['duration_s'] = all_shifts['duration'].fillna('00:00').apply(str_to_sec)
+    all_shifts['startTime_s'] = all_shifts['startTime'].apply(str_to_sec) + 60 * (all_shifts['period'] - 1) * 20
+    all_shifts['endTime_s'] = all_shifts['endTime'].apply(str_to_sec) + 60 * (all_shifts['period'] - 1) * 20
+    
+    
+
+    # all_shifts['date'] = pbp_json['gameDate']
+    # all_shifts['season'] = pbp_json['season']
+    # all_shifts['gameType'] = game_id
+
+
+    return all_shifts
+
+# /Users/max/Documents/Projects/max_nhl_scraper/.venv/bin/python -m nhl.utility.functions
+
+# print(fetch_html_shifts(game_id=2023020069))
